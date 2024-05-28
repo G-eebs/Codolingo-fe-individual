ab879dc313ee664be4b58e2d31555a4462579791
George B <george.bartram29@gmail.com>
2024-05-28 11:59:50 +0100

    adds progress circles to lessons using user data

--- app/(tabs)/Home/index.jsx
+++ app/(tabs)/Home/index.jsx
@@ -1,90 +1,121 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Link } from "expo-router";
 import { useState, useEffect } from "react";
 import { getAllLessons } from "../../../utils/utils";
+import { UserContext } from "../../../contexts/User";
+import CircularProgress from "react-native-circular-progress-indicator";
 const MainImage = require("../../../assets/geoffrey.jpg");
 
 export default function Home() {
-  const [allLessons, setAllLessons] = useState([]);
-  const [loading, setLoading] = useState(true);
+	const [allLessons, setAllLessons] = useState([]);
 
-  useEffect(() => {
-    getAllLessons()
-      .then((response) => {
-        setAllLessons(response.data.lessons);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setLoading(false);
-      });
-  }, []);
+	const [loading, setLoading] = useState(true);
 
-  if (loading) {
-    return <Text>Loading...</Text>;
+	const { user } = useContext(UserContext);
+
+	useEffect(() => {
+		setLoading(true);
+		getAllLessons()
+			.then((response) => {
+				setAllLessons(response.data.lessons);
+				setLoading(false);
+			})
+			.catch((error) => {
+				console.log(error);
+				setLoading(false);
+			});
+	}, []);
+
+	if (loading) {
+		return <Text style={[styles.text, styles.loading]}>Loading...</Text>;
+	}
+
+  function calculateLessonProgress(lessonQuestions, userProgress) {
+    const completedQuestions = lessonQuestions.filter(question => userProgress.includes(question))
+    return completedQuestions.length / lessonQuestions.length * 100
   }
 
-  return (
-    <>
-      <View style={styles.header}>
-        <Text style={[styles.text, styles.title]}>
-          Click on a lesson to begin...
-        </Text>
-        <Image style={styles.image} source={MainImage} />
-      </View>
+	return (
+		<View style={styles.page}>
+			<View style={styles.header}>
+				<Text style={[styles.text, styles.title]}>{"Click on a lesson\nto begin..."}</Text>
+				<Image style={styles.image} source={MainImage} />
+			</View>
 
-      <View style={styles.circles}>
-        {allLessons.map((lesson) => {
-          return (
-            <Link
-              key={lesson._id}
-              style={styles.circle}
-              href={`/Home/${lesson._id}`}
-            >
-              {lesson._id}
-            </Link>
-          );
-        })}
-      </View>
-    </>
-  );
+			<View style={styles.lessons}>
+				{allLessons.map((lesson) => {
+					return (
+						<Link key={lesson._id} style={styles.lesson} href={`/Home/${lesson._id}`}>
+							<CircularProgress
+								value={calculateLessonProgress(lesson.questions, user.progress)}
+								title={lesson._id}
+                radius={30}
+                titleFontSize={18}
+                circleBackgroundColor={'#333'}
+								showProgressValue={false}
+								style={styles.progressCircle}
+							></CircularProgress>
+						</Link>
+					);
+				})}
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  text: {
-    fontFamily: "monospace",
-    fontSize: 20,
+  page : {
+    height: "100%",
+    backgroundColor: "#bbb",
   },
-  circle: {
-    backgroundColor: "grey",
-    width: 50,
-    height: 50,
-    borderRadius: 25,
+
+	text: {
+		fontFamily: "monospace",
+		fontSize: 20,
+	},
+
+	header: {
+		padding: 15,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+    backgroundColor: "#aaa",
+	},
+
+	title: {},
+
+	image: {
+		height: 120,
+		width: 120,
+		borderColor: "grey",
+		borderWidth: 4,
+		borderRadius: 10,
+	},
+
+	lessons: {
+		flexDirection: "column",
+		alignItems: "center",
+	},
+
+	progressCircle: {
+		width: 57,
+		height: 57,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "yellow",
+		textAlign: "center",
+	},
+
+	lesson: {
     marginTop: 15,
-    fontFamily: "monospace",
-    fontSize: 20,
-    textAlign: "center",
-    padding: 13,
-  },
+    height: 60,
+	},
 
-  header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-
-  image: {
-    height: 120,
-    width: 120,
-    borderColor: "grey",
-    borderWidth: 4,
-    borderRadius: 10,
-  },
-
-  circles: {
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
+	loading: {
+		marginTop: 15,
+		textAlign: "center",
+	},
 });
--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -9,7 +9,7 @@ const MainImage = require("../../assets/geoffrey.jpg");
 export default function Start() {
 	const { user, setUser } = useContext(UserContext);
 
-	const [userNameInput, setUserNameInput] = useState("thompsurn");
+	const [userNameInput, setUserNameInput] = useState("cogger101");
 
 	const [passwordInput, setPasswordInput] = useState("password");
 
